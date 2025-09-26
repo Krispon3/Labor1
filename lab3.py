05cccb68531e9fa7c58411ed29f86f28642edb73
Krispon3 <aniota-aniota2@mail.ru>
2025-09-26 12:53:15 +0400

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -4,17 +4,17 @@ x = -5
 
 while (x <= 0):
     y = (x**4)*(2**x)
-    print("x = ",x,"y = ", y)
+    print("x = ",x, "y = ", y)
     x += 0.5
 
 while x > 0 and x <= pi / 2 :
     y = (sin(x))**cos(x)
-    print("x = ",x,"y = ", y)
+    print("x = ", x, "y = ", y)
     x += 0.5
 
 while x > pi/2:
     y = x**log(x)
-    print("x = ",x,"y = ", y)
+    print("x = ",x, "y = ", y)
     if x == 5:
         break
     else:
